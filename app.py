80e73aaa725c8e76256d12d07a33a727598b8d20
kiko <95848578+K1K04@users.noreply.github.com>
2024-04-16 10:29:32 +0200

    Archivo app.py actualizado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, abort, redirect
+from flask import Flask, render_template, request, abort, redirect  
 import json
 
 app = Flask(__name__)
@@ -46,6 +46,23 @@ def lista_orype():
         resultados = datos
 
     return render_template('listaorype.html', resultados=resultados, search_term=search_term)
+
+@app.route('/producto/<int:id>')
+def mostrar_producto(id):
+    # Buscar el producto con el id dado en tus datos
+    producto_encontrado = None
+    for producto in datos:
+        if producto['id'] == id:
+            producto_encontrado = producto
+            break
+
+    # Verificar si se encontró el producto
+    if producto_encontrado:
+        return render_template('producto.html', producto=producto_encontrado)
+    else:
+        # Si el producto no se encuentra, puedes renderizar una plantilla de error o redirigir a otra página.
+        return render_template('error.html', message='Producto no encontrado'), 404
+
 
 
 if __name__ == '__main__':
