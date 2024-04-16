48b03ec2409447ab83576a35ca3661f9efc4bc3c
kiko <95848578+K1K04@users.noreply.github.com>
2024-04-16 09:08:08 +0200

    Modificacion del app.py
    Para ajustarse y poder buscar los product con el json

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, abort, redirect
+import json
 
 app = Flask(__name__)
+
+# Carga los datos del archivo JSON
+with open('/media/usuario/KIKO/PaginaPC/datos.json', 'r', encoding='utf-8') as json_file:
+    datos = json.load(json_file)
 
 @app.route('/')
 def index():
@@ -14,10 +19,6 @@ def Computer():
 def Rams():
     return render_template("Rams.html")
 
-@app.route('/Laptop')
-def Laptop():
-    return render_template("laptop.html")
-
 @app.route('/Products')
 def Products():
     return render_template("products.html")
@@ -29,6 +30,23 @@ def About():
 @app.route('/Contact')
 def Contact():
     return render_template("contact.html")
+
+@app.route('/listaorype', methods=['POST'])
+def lista_orype():
+    # Obtener la cadena de búsqueda del formulario
+    search_term = request.form.get('search_term', '')
+
+    # Filtrar los productos según la cadena de búsqueda
+    resultados = []
+    if search_term:
+        for producto in datos:
+            if producto['titulo'].startswith(search_term):
+                resultados.append(producto)
+    else:
+        resultados = datos
+
+    return render_template('listaorype.html', resultados=resultados, search_term=search_term)
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000, debug=True)
