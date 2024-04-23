21b381e3682714caaa676aab77a07fa4b5116a0a
kiko <95848578+K1K04@users.noreply.github.com>
2024-04-23 08:39:55 +0200

    final ejercicios extra

--- app.py
+++ app.py
@@ -11,41 +11,25 @@ with open('/media/usuario/KIKO/PaginaPC/datos.json', 'r', encoding='utf-8') as j
 def index():
     return render_template("index.html")
 
-@app.route('/Computer')
-def Computer():
-    return render_template("Computer.html")
-
-@app.route('/Rams')
-def Rams():
-    return render_template("Rams.html")
-
-@app.route('/Products')
-def Products():
-    return render_template("products.html")
-
-@app.route('/About')
-def About():
-    return render_template("about.html")
-
-@app.route('/Contact')
+@app.route('/Contact', methods=['GET', 'POST'])
 def Contact():
-    return render_template("contact.html")
+    # Obtener la marca seleccionada del formulario
+    selected_brand = request.form.get('selected_brand', '')
 
-@app.route('/listaorype', methods=['POST'])
-def lista_orype():
-    # Obtener la cadena de búsqueda del formulario
-    search_term = request.form.get('search_term', '')
-
-    # Filtrar los productos según la cadena de búsqueda
+    # Filtrar los productos según la marca seleccionada
     resultados = []
-    if search_term:
+    if selected_brand:
         for producto in datos:
-            if producto['titulo'].startswith(search_term):
+            if selected_brand.lower() in producto['marca'].lower():
                 resultados.append(producto)
     else:
         resultados = datos
 
-    return render_template('listaorype.html', resultados=resultados, search_term=search_term)
+    # Obtener las marcas únicas para el select
+    marcas_unicas = set(producto['marca'] for producto in datos)
+
+    return render_template('contact.html', resultados=resultados, selected_brand=selected_brand, marcas_unicas=marcas_unicas)
+
 
 @app.route('/producto/<int:id>')
 def mostrar_producto(id):
@@ -63,8 +47,5 @@ def mostrar_producto(id):
         # Si el producto no se encuentra, puedes renderizar una plantilla de error o redirigir a otra página.
         return render_template('error.html', message='Producto no encontrado'), 404
 
-
-
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000, debug=True)
-
