c7ea0d74e4055d6d6a56c8bfb14bf4b8865153ff
kiko <95848578+K1K04@users.noreply.github.com>
2024-04-23 09:05:13 +0200

    correcion final de la bsuqueda de  productos

--- app.py
+++ app.py
@@ -13,38 +13,37 @@ def index():
 
 @app.route('/Contact', methods=['GET', 'POST'])
 def Contact():
-    # Obtener la marca seleccionada del formulario
+    search_term = request.form.get('search_term', '')
     selected_brand = request.form.get('selected_brand', '')
-
-    # Filtrar los productos según la marca seleccionada
     resultados = []
-    if selected_brand:
+    if search_term and selected_brand:
+        for producto in datos:
+            if search_term.lower() in producto['titulo'].lower() and selected_brand.lower() in producto['marca'].lower():
+                resultados.append(producto)
+    elif search_term:
+        for producto in datos:
+            if search_term.lower() in producto['titulo'].lower():
+                resultados.append(producto)
+    elif selected_brand:
         for producto in datos:
             if selected_brand.lower() in producto['marca'].lower():
                 resultados.append(producto)
     else:
         resultados = datos
-
-    # Obtener las marcas únicas para el select
     marcas_unicas = set(producto['marca'] for producto in datos)
 
-    return render_template('contact.html', resultados=resultados, selected_brand=selected_brand, marcas_unicas=marcas_unicas)
-
+    return render_template('contact.html', resultados=resultados, search_term=search_term, selected_brand=selected_brand, marcas_unicas=marcas_unicas)
 
 @app.route('/producto/<int:id>')
 def mostrar_producto(id):
-    # Buscar el producto con el id dado en tus datos
     producto_encontrado = None
     for producto in datos:
         if producto['id'] == id:
             producto_encontrado = producto
             break
-
-    # Verificar si se encontró el producto
     if producto_encontrado:
         return render_template('producto.html', producto=producto_encontrado)
     else:
-        # Si el producto no se encuentra, puedes renderizar una plantilla de error o redirigir a otra página.
         return render_template('error.html', message='Producto no encontrado'), 404
 
 if __name__ == '__main__':
